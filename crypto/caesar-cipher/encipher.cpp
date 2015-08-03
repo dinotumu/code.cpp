d3ea7d1330488fa4d3b69a50bffc3f749ff8f73d
Sai Dinesh Tumu <dinoRkz@outlook.com>
2015-08-04 04:10:04 +0530

    caesar-cipher

---
+++ crypto/caesar-cipher/encipher.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char caesar( char c , int k )
+{
+    if( c >= 65 && c <= 90 )
+        c = (((c-65)+k) % 26) + 65;
+    else if( c >= 97 && c <= 122 )
+        c = (((c-97)+k) % 26) + 97;
+    return c;
+}
+
+int main()
+{
+    string input;
+    int k;
+    cin>>input;
+    cin>>k;
+    string output = "";
+    for(int x = 0; x < input.length(); x++)
+        output += caesar(input[x], k);
+    cout << output << endl;
+}
+
